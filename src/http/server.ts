2f75821e4825df6481ecfcfbf128ab4a65e754ae
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-09-12 10:54:16 -0300

    feat: add getWeekSummary function

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,16 +6,23 @@ import {
 } from 'fastify-type-provider-zod'
 import { createCompletionRoute } from './routes/create-completion'
 import { createGoalRoute } from './routes/create-goal'
-import { getPendingGoal } from './routes/get-pending.goals'
+import { getPendingGoalRoute } from './routes/get-pending.goals'
+import { getWeeksumaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
-app.register(getPendingGoal)
+app.register(getPendingGoalRoute)
+app.register(getWeeksumaryRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server runing!')
