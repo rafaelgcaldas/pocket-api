05afe035fbc035bdb369773e2debfd2bae2c9a91
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-09-12 10:52:24 -0300

    fix: fix firstDayOfWeek variable name

--- src/functions/get-week-pending-goal.ts
+++ src/functions/get-week-pending-goal.ts
@@ -4,7 +4,7 @@ import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 
 export async function getWeekPendingGoals() {
-  const fistDayOfWeek = dayjs().startOf('week').toDate()
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
   const lastDayOfWeek = dayjs().endOf('week').toDate()
 
   const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
@@ -28,7 +28,7 @@ export async function getWeekPendingGoals() {
       .from(goalCompletions)
       .where(
         and(
-          gte(goalCompletions.createdAt, fistDayOfWeek),
+          gte(goalCompletions.createdAt, firstDayOfWeek),
           lte(goalCompletions.createdAt, lastDayOfWeek)
         )
       )
